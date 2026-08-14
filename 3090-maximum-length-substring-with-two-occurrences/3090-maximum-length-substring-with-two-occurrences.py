bda761244f37ec0476fb0cb2621ae258dc51fd76
Rohitkc8 <rohitkc295@gmail.com>
2026-08-14 22:13:15 +0530

    Time: 3 ms (76.16%), Space: 19.1 MB (88.83%) - LeetHub

---
+++ 3090-maximum-length-substring-with-two-occurrences/3090-maximum-length-substring-with-two-occurrences.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maximumLengthSubstring(self, s: str) -> int:
+        freq = {}
+        l = 0
+        ans = 0
+
+        for r in range(len(s)):
+            freq[s[r]] = freq.get(s[r], 0) + 1
+
+            while freq[s[r]] > 2:
+                freq[s[l]] -= 1
+                l += 1
+
+            ans = max(ans, r - l + 1)
+
+        return ans
