da314f933e18c3c6e356ab87741a0cbeed9b869f
Rohitkc8 <rohitkc295@gmail.com>
2026-08-26 10:38:04 +0530

    Time: 0 ms (100%), Space: 19.5 MB (34.98%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,7 +1,19 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        p=[]
-        from itertools import permutations
-        for i in permutations(nums):
-            p.append(i)
-        return p
+        used=[0]*(len(nums))
+        ans=[]
+        def bac(sol):
+            if len(sol)==len(nums):
+                ans.append(sol.copy())
+                return
+            for i in range(len(nums)):
+                if used[i]==1:
+                    continue
+                used[i]=1
+                sol.append(nums[i])
+                bac(sol)
+                used[i]=0
+                sol.pop()
+        bac([])
+        return ans
+            
